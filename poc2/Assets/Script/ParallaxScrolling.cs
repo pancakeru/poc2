aba6e803beaf279d90a1ced68deb86a7a331c7da
Arnav Arora <arnavarora2016@gmail.com>
2024-12-11 18:09:37 -0500

    ParrlaxScroll

--- poc2/Assets/Script/ParallaxScrolling.cs
+++ poc2/Assets/Script/ParallaxScrolling.cs
@@ -17,13 +17,9 @@ public class ParallaxScrolling : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-<<<<<<< HEAD
-        float xdistance = (Maincamera.transform.position.x - XstartPos) * XparallaxEffect;
-        float yPosition = YstartPos;//Maincamera.transform.position.y; //+ YstartPos /2 ;
-        transform.position = new Vector3(XstartPos + xdistance, yPosition, transform.position.z);
-=======
+
         float xdistance = (Maincamera.transform.position.x ) * XparallaxEffect;
         transform.position = new Vector3(XstartPos + xdistance, transform.position.y, transform.position.z);
->>>>>>> 57ad9bee6f36e59b02f34332c44c61b093348397
+
     }
 }
